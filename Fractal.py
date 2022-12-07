e65582aac694843324c838dbcf12c055c1175b84
IlanProv <559644@my.cuhsd.org>
2022-12-07 09:58:10 -0800

    added Y tree

--- Fractal.py
+++ Fractal.py
@@ -3,10 +3,59 @@ import turtle as trtl
 #Settings
 
 #setup
-wn=trtl.escreen()
-wn.setup(width=1.0, hight=1.0)
+wn=trtl.Screen()
+wn.setup(width=1.0, height=1.0)
 #trutles
 
+fractalMaker=trtl.Turtle()
+  
+  
+fractalMaker.speed(0)
+  
+# turning the turtle to face upwards
+fractalMaker.rt(-90)
+  
+# the acute angle between
+# the base and branch of the Y
+angle = 30
+  
+# function to plot a Y
+def y(sz, level):   
+  
+    if level > 0:
+        fractalMaker.colormode(255)
+          
+        # splitting the rgb range for green
+        # into equal intervals for each level
+        # setting the colour according
+        # to the current level
+        fractalMaker.pencolor(0, 255//level, 0)
+          
+        # drawing the base
+        fractalMaker.fd(sz)
+  
+        fractalMaker.rt(angle)
+  
+        # recursive call for
+        # the right subtree
+        y(0.8 * sz, level-1)
+          
+        fractalMaker.pencolor(0, 255//level, 0)
+          
+        fractalMaker.lt( 2 * angle )
+  
+        # recursive call for
+        # the left subtree
+        y(0.8 * sz, level-1)
+          
+        fractalMaker.pencolor(0, 255//level, 0)
+          
+        fractalMaker.rt(angle)
+        fractalMaker.fd(-sz)
+           
+          
+# tree of size 80 and level 7
+y(80, 7)
 
 #functions
 
