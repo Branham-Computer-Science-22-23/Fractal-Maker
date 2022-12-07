88875c57dc4da150afd00c02035d00eb970f8d7a
SuChoy <559459@my.cuhsd.org>
2022-12-07 09:38:11 -0800

    User input

--- Fractal.py
+++ Fractal.py
@@ -1,14 +1,17 @@
 #imports
 import turtle as trtl
 #Settings
-
+print("Welcome to the Fractal Maker! \n Here you can make different designs and customize them to your liking.")
 #setup
-wn=trtl.escreen()
-wn.setup(width=1.0, hight=1.0)
+wn=trtl.Screen()
+wn.setup(width=1.0, height=1.0)
 #trutles
 
-
 #functions
+#ask user for input
+ColorChoice = input("What color would you like? ")
+NumIterations = input(int("How many iterations would you like? "))
+FranctalKind = input("What kind of Fractal? ")
 
 #events
 wn.mainloop()
